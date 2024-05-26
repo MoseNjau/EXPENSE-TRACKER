2ae6c44541abeae72aab88b4cf97d4fcdc9e2a34
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-26 22:19:33 +0300

    feat: add dashboard components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,40 @@ import styled from "styled-components";
 import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./components/orb/Orb";
+import Navigation from "./components/navigation/Navigation";
+import { useMemo, useState } from "react";
+import Dashboard from "./components/dashboard/Dashboard";
+import Income from "./components/income/Income";
+import Expenses from "./components/expenses/Expenses";
+import { useGlobalContext } from "./context/GlobalContext";
 
 function App() {
+  const [active, setActive] = useState(1);
+  const global = useGlobalContext()
+  console.log(global);
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
   return (
     <AppStyled bg={bg} className="App">
-      <Orb />
+      {orbMemo}
       <MainLayout>
-
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -18,6 +45,17 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${(props) => props.bg});
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
 
 export default App;
