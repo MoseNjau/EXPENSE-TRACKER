9dc6039445a9a7ca2c7b8b85225c29fd161de16b
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-05-27 07:12:35 +0300

    modified changes in transactions

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -7,13 +7,3 @@ router.post('/add-income', addIncome)
         .delete('/delete-income/:id', deleteIncome);
 
 module.exports = router;
-
-const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
-
-const router = require('express').Router()
-
-router.post('/add-income', addIncome)
-    .get('/get-incomes', getIncomes)
-    .delete('/delete-income/:id', deleteIncome)
-
-module.exports = router;
